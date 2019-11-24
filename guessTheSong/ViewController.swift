8abb7e9fe76fcf8b6e55480e3289bb35a3d1abf8
bruce11521 <bruce11521@gmail.com>
2019-11-24 16:30:54 +0800

    Initial Commit

---
+++ guessTheSong/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  guessTheSong
+//
+//  Created by 林伯翰 on 2019/11/24.
+//  Copyright © 2019 Bruce Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
